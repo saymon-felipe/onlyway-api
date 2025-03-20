f0d03e62a36874d3d058abf52d59a87a9bfe7139
Saymon Felipe <80544520+saymon-felipe@users.noreply.github.com>
2025-03-20 15:24:47 -0300

    Tarefa #653

--- routes/utils.js
+++ routes/utils.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const uploadMusics = require("../utils/uploadMusics");
 const functions = require("../utils/functions");
 
-router.get("/upload_musics/:token", async (req, res) => {
+router.post("/upload_musics/:token", async (req, res) => {
     const token = req.params.token;
 
     if (!token == process.env.JWT_KEY) {
         return res.status(401).send("Não autorizado.");
     } else {
-        uploadMusics.upload().then(() => {
+        uploadMusics.upload(req.body.musics).then(() => {
             let response = functions.createResponse("Upload das músicas feito com sucesso", null, "GET", 200);
             return res.status(200).send(response);
         });
--- utils/uploadMusics.js
+++ utils/uploadMusics.js
@@ -1,15 +1,14 @@
 const functions = require("../utils/functions");
 
 let uploadMusics = {
-    upload: function () {
+    upload: function (musics) {
         return new Promise((resolve, reject) => {
-            const musicas = require("../public/music_data.json");
             let insertMusics = "";
 
-            for (let i = 0; i < musicas.length; i++) {
-                let currentMusic = musicas[i];
+            for (let i = 0; i < musics.length; i++) {
+                let currentMusic = musics[i];
 
-                if (i == musicas.length - 1) {
+                if (i == musics.length - 1) {
                     insertMusics += `(${currentMusic.artist.indexOf("Unknown") == -1 ? '"' + currentMusic.artist + '"' : null}, ${currentMusic.title.indexOf("Unknown") == -1 ? '"' + currentMusic.title + '"' : '"' + currentMusic.file_name + '"'}, ${currentMusic.album.indexOf("Unknown") == -1 && currentMusic.album != "no title" ? '"' + currentMusic.album + '"' : null}, ${currentMusic.year}, ${currentMusic.genre.indexOf("Unknown") == -1 ? '"' + currentMusic.genre + '"' : null})`;
                 } else {
                     insertMusics += `(${currentMusic.artist.indexOf("Unknown") == -1 ? '"' + currentMusic.artist + '"' : null}, ${currentMusic.title.indexOf("Unknown") == -1 ? '"' + currentMusic.title + '"' : '"' + currentMusic.file_name + '"'}, ${currentMusic.album.indexOf("Unknown") == -1 && currentMusic.album != "no title" ? '"' + currentMusic.album + '"' : null}, ${currentMusic.year}, ${currentMusic.genre.indexOf("Unknown") == -1 ? '"' + currentMusic.genre + '"' : null}),`;
@@ -18,7 +17,7 @@ let uploadMusics = {
 
             functions.executeSql(
                 `
-                    INSERT INTO musicas
+                    INSERT IGNORE INTO musicas
                     (artista, nome, album, ano, genero)
                     VALUES
                     ${insertMusics}
